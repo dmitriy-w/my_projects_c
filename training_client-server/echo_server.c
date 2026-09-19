e47f80b68bf07436492e3ba00121e041c84a5cb5
dmitriy-w <dimapoteruhin2504@gmail.com>
2026-09-19 23:18:12 +0500

    Спринт 1: Инициализация сокета и привязка к порту через bind

---
+++ training_client-server/echo_server.c
@@ -0,0 +1,33 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main() {
+    int listenfd, connfd; //listenfd - это слушающий сокет (дескриптор), connfd - это присоединённый сокет
+    socklen_t clilen; //размер структуры адреса клиента
+    struct sockaddr_in cliaddr, servaddr; //servaddr — это настройки адреса сервера (какой порт слушать),  cliaddr - это IP-адрес и порт клиента
+
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listenfd < 0) {
+        perror("Ошибка - не получилось создать socket");
+        exit(1);
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); //32-разрядный адрес ipv4
+    servaddr.sin_port = htons(4444); //16-разрядный номер порта ТСР
+
+    bind(listenfd, (const struct sockaddr *) &servaddr, sizeof(servaddr));
+    if ( < 0) {
+        perror("Ошибка в bind");
+        exit(1);
+    }
+
+    return 0;
+}
